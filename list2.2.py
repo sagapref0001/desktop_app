c249740af53c0ceeb38e156536ad0f61b6a3c605
sagapref0001 <sagapref0001@gmail.com>
2023-09-30 22:39:16 +0900

    ファイルの読み込みと移動

--- list2.2.py
+++ list2.2.py
@@ -8,19 +8,3 @@ root.geometry("250x100")
 
 # ウインドウ状態の維持
 root.mainloop()
-
-
-# ###関数###
-# #関数の定義
-# def run_func():
-#     print("Hello!!")
-#     print("OK")
-#
-# ###GUI###
-# #ウインドウのサイズ指定
-# root.geometry("250x100")
-# #Runのボタン
-# run_button = tk.Button(root,text = "Run",command = run_func)
-# run_button.place(x=100,y=30)
-# root.mainloop()
-

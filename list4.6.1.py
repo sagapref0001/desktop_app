097d7810923034fc7e7547fe668d00c12fbd5f4a
sagapref0001 <sagapref0001@gmail.com>
2023-10-08 22:50:02 +0900

    メニューバーの設置

--- list4.6.1.py
+++ list4.6.1.py
@@ -20,4 +20,23 @@ yscroll.pack(side=tk.RIGHT,fill=tk.Y)
 # テキストボックスの配置
 txtbox.pack()
 
+# メニューバーの作成
+menubar = tk.Menu(root)
+root.configure(menu=menubar)
+# Fileメニュー
+filemenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label = "File",menu=filemenu)
+# Fileメニューの内容
+filemenu.add_command(label= "Open...")
+filemenu.add_command(label = "Save as...")
+filemenu.add_separator()
+filemenu.add_command(label="Exit",command=lambda :root.destroy())
+
+# Helpメニュー
+helpmenu = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Help",menu=helpmenu)
+
+# >Manual
+helpmenu.add_command(label="Manual")
+
 root.mainloop()
